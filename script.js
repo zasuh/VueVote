7b19ac40edf4fd5b72cdf53156380f25f6da7288
Zane Suhadolnik <zanesuhi@gmail.com>
2018-05-26 08:01:14 +0200

    Added ability to edit entries, added vote counter to determine winner

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let app = new Vue({
     el: '#app',
     data: {
+        editMode: false,
         frameworks: [
             { name: 'Vue', votes: 0 },
             { name: 'Angular', votes: 0 },
@@ -17,6 +18,35 @@ let app = new Vue({
                 votes: 0
             })
             event.target.value = ''
+        },
+        remove: function(f) {
+            this.frameworks = this.frameworks.filter(i => i !=f)
+        },
+        load: function () {
+            let data = localStorage.setItem('saved')
+            if (data) {
+                this.frameworks = JSON.parse(data)
+            }
+        },
+        save: function() {
+            let data = JSON.stringify(this.frameworks)
+            localStorage.setItem('saved', data)
+        },
+        toggleEditMode: function() {
+            this.editMode = !this.editMode
         }
+    },
+
+    computed: {
+        winnerString: function() {
+            let scores = this.frameworks.map(f => f.votes)
+            let highscore = Math.max.apply(Math, scores)
+            let bestList = this.frameworks.filter(f => f.votes == highscore)
+            let bestNames = bestList.map(f => f.name)
+            return bestNames.join(', ')
+        }
+    },
+    created: function() {
+        this.load()
     }
 })
